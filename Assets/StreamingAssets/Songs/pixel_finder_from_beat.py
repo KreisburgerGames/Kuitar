ff406c217ca106f105223bb0dabb5d5cc03b2658
Kreis <kreisburgergames@gmail.com>
2024-10-26 16:42:28 -0400

    include mapping tools in StreamingAssets

---
+++ Assets/StreamingAssets/Songs/pixel_finder_from_beat.py
@@ -0,0 +1,16 @@
+pixels_per_beat = 4
+beats_per_bar = 4
+offset = 0
+
+while True:
+    bar = int(input("bar: "))
+    beat = int(input("beat: "))
+    tick = int(input("tick: "))
+    subtick = int(input("sub-tick: "))
+
+    pixel = ((bar - 1) * (pixels_per_beat * beats_per_bar)) + (beat * pixels_per_beat) + tick - pixels_per_beat - 1 + subtick - 1 - offset
+    print(pixel)
+
+    i = input("Press Enter to enter again. Enter Q to quit. ")
+    if (i == "Q" or i == "q"):
+        exit()
---
+++ Assets/StreamingAssets/Songs/pixel_finder_timecode.py
@@ -0,0 +1,15 @@
+bpm = 155
+pixelsPerBeat = 32
+secondsPerBeat = 60 / bpm
+offset = 0
+
+while True:
+	time = float(input("time: "))
+
+	beat = time / secondsPerBeat
+
+	print(round(beat * pixelsPerBeat) - offset)
+
+	i = input("Press Enter to enter again. Enter Q to quit. ")
+	if (i == "Q" or i == "q"):
+		exit()
