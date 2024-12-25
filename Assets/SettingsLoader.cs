8a22151d952bc14aea8bda478a35ffca16e12d69
Kreis <kreisburgergames@gmail.com>
2024-12-25 00:59:17 -0500

    fix settings

--- Assets/SettingsLoader.cs
+++ Assets/SettingsLoader.cs
@@ -14,6 +14,8 @@ public class SettingsLoader : MonoBehaviour
     public int refreshRate;
     public float latency;
     public static SettingsLoader instance;
+    public int frameLimit;
+    public int vSync;
 
     private void Awake()
     {
@@ -45,12 +47,30 @@ public class SettingsLoader : MonoBehaviour
         }
         if (PlayerPrefs.HasKey("Latency"))
         {
-            refreshRate = PlayerPrefs.GetInt("Latency");
+            latency = PlayerPrefs.GetFloat("Latency");
         }
         else
         {
             PlayerPrefs.SetFloat("Latency", 250f);
             latency = PlayerPrefs.GetFloat("Latency");
+        }
+        if (PlayerPrefs.HasKey("FrameLimit"))
+        {
+            frameLimit = PlayerPrefs.GetInt("FrameLimit");
+        }
+        else
+        {
+            PlayerPrefs.SetInt("FrameLimit", (int)Screen.mainWindowDisplayInfo.refreshRate.value);
+            frameLimit = PlayerPrefs.GetInt("FrameLimit");
+        }
+        if (PlayerPrefs.HasKey("VSync"))
+        {
+            vSync = PlayerPrefs.GetInt("VSync");
+        }
+        else
+        {
+            PlayerPrefs.SetInt("VSync", 1);
+            vSync = PlayerPrefs.GetInt("VSync");
         }
 
         FullScreenMode fullScreenMode;
@@ -64,6 +84,8 @@ public class SettingsLoader : MonoBehaviour
         }
         
         Screen.SetResolution(screenWidth, screenHeight, fullScreenMode);
+        Application.targetFrameRate = frameLimit;
+        QualitySettings.vSyncCount = vSync;
     }
 
     public static bool ToBool(int value)
