80ec0f6853d1674b9bdc7ea6dbfe87d80ce9952c
Kreis <kreisburgergames@gmail.com>
2025-03-23 23:20:08 -0400

    note velocity is now static from root note

--- Assets/Note.cs
+++ Assets/Note.cs
@@ -48,6 +48,9 @@ public class Note : MonoBehaviour
     private int indexOffset;
 
     public float noteVelCalculationEndDist;
+    private bool setNoteVel;
+    public static Note Root;
+    public float noteVel;
 
     // 1 = High, 4 = Low
     [Range(1, 4)]
@@ -217,6 +220,11 @@ public class Note : MonoBehaviour
         {
             if(downStrum) bgSprite.sprite = DownStrumIMG; else bgSprite.sprite = UpStrumIMG;
         }
+        
+        if(index == 0)
+        {
+            Root = this;
+        }
     }
 
     public void GetNB(int i)
@@ -275,11 +283,25 @@ public class Note : MonoBehaviour
     {
         if(!moving) return;
 
-        float distFromTarget = targetPos.x - transform.position.x /*- .0219f*/;
-        if(distFromTarget <= 1.219f) return;
-        double secondsAvailable = (beat * conductor.secondsPerBeat) - conductor.songPos;
-        vel = new Vector2(distFromTarget / (float)secondsAvailable, 0);
-        rb.velocity = vel;
+        if(!setNoteVel)
+        {
+            if(Root == this)
+            {
+                float distFromTarget = targetPos.x - transform.position.x /*- .0219f*/;
+                //if(distFromTarget <= 1.35f) return;
+                double secondsAvailable = (beat * conductor.secondsPerBeat) - conductor.songPos;
+                vel = new Vector2(distFromTarget / (float)secondsAvailable, 0);
+                rb.velocity = vel;
+            }
+            else
+            {
+                rb.velocity = Root.vel;
+            }
+            setNoteVel = true;
+        }
+        
+
+        if(index == 0) Debug.Log(rb.velocity.x);
 
         if(hammerStart)
         {
